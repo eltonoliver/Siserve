5f79e16ff28562bd231013066fc70c924f12e419
Elton Oliveira <eltonknoxville@hotmail.com>
2016-11-21 07:43:05 -0400

    InterfaceHome
    InterfaceHome

--- app/src/main/java/com/app/siserve/siserve/LoginActivity.java
+++ app/src/main/java/com/app/siserve/siserve/LoginActivity.java
@@ -3,6 +3,7 @@ package com.app.siserve.siserve;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -93,6 +94,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Intent t = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(t);
 
         emailT = (AutoCompleteTextView) findViewById(R.id.email);
         btnLogin = (Button) findViewById(R.id.email_sign_in_button);
